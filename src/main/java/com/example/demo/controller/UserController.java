2856d82d71d7a2056a898b69843a3e1727ff6a5e
DauThuy <dauthithuy99@gmail.com>
2021-07-14 21:55:04 +0700

    [java][week4][manage account] Fix anotation CrossOrigin

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,22 +14,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "*")
+
 public class UserController {
     @Autowired
     private AccountService accountService;
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/login")
     public InfoDto getInfo(@Valid @RequestBody AccountDto dto) {
             return accountService.login(dto);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value="/admin/users")
     public ResponseEntity<?> getListUsers() {
         List<Account> users = accountService.getAllUser();
         return ResponseEntity.ok(users);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value="/{id}")
     public ResponseEntity<?> getUsersById(@PathVariable int id) {
         System.out.println(id);
@@ -37,17 +40,20 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/create")
     public ResponseEntity<?> createUser() {
         return null;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/update/{id}")
     public ResponseEntity<?> updateUser(@Valid @RequestBody UpdateUserReq req, @PathVariable int id) {
         Account account = accountService.updateUser(req, id);
         return ResponseEntity.ok(account);
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("delete/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable int id) {
         accountService.deleteUserById(id);
